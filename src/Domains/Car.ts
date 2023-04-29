0ceae68dd904fb19652d83ce06118ac6cb958359
mirelladourado <mirellaalvesdourado@gmail.com>
2023-04-29 16:05:59 -0300

    fix: (Requisito 1) Domain Car - modificadores de acesso

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,14 +1,14 @@
 import ICar from "../Interfaces/ICar"
 
 class Car {
-  private id: string | undefined
-  private model: string
-  private year: number
-  private color: string
-  private status: boolean
-  private buyValue: number
-  protected doorsQty: number
-  protected seatsQty: number
+  protected id: string | undefined
+  protected model: string
+  protected year: number
+  protected color: string
+  protected status: boolean
+  protected buyValue: number
+  private doorsQty: number
+  private seatsQty: number
 
   constructor(car: ICar) {
     this.id = car.id
