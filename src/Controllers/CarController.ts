38d3d92e0ede9609e96ece295008d41a70bea131
mirelladourado <mirellaalvesdourado@gmail.com>
2023-04-29 15:47:07 -0300

    feat: (Requisito 1) CarController - create

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import CarService from '../Services/CarService';
+
+export default class CarController {
+  private req: Request;
+  private res: Response;
+  private service: CarService;
+
+  constructor(req: Request, res: Response) {
+    this.req = req;
+    this.res = res;
+    this.service = new CarService();
+  }
+
+  public async create() {
+    const car = this.req.body
+    try {
+      const newCar = await this.service.create(car);
+      return this.res.status(201).json(newCar)
+    } catch (error) {
+      return this.res.status(500).json({ message: error })
+    }
+  }
+}
