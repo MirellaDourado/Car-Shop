a2747dcccd253ab105efc45527297999e17115e1
mirelladourado <mirellaalvesdourado@gmail.com>
2023-04-29 18:18:06 -0300

    feat: (Requisito 5) Motocycle Domain

---
+++ src/Domains/Motocycle.ts
@@ -0,0 +1,89 @@
+import IMotocycle from '../Interfaces/IMotocycle';
+
+class Motocycle {
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status: boolean;
+  protected buyValue: number;
+  private category: string;
+  private engineCapacity: number;
+
+  constructor(moto: IMotocycle) {
+    this.id = moto.id;
+    this.model = moto.model;
+    this.year = moto.year;
+    this.color = moto.color;
+    this.status = moto.status || false;
+    this.buyValue = moto.buyValue;
+    this.category = moto.category;
+    this.engineCapacity = moto.engineCapacity;
+  }
+
+  public setId(id: string) {
+    this.id = id;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public setModel(model: string) {
+    this.model = model;
+  }
+
+  public getModel() {
+    return this.model;
+  }
+
+  public setYear(year: number) {
+    this.year = year;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+
+  public setColor(color: string) {
+    this.color = color;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public setStatus(status: boolean) {
+    this.status = status;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public setBuyValue(buyValue: number) {
+    this.buyValue = buyValue;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }  
+
+  public setCategory(category: string) {
+    this.category = category;
+  }
+
+  public getCategory() {
+    return this.category;
+  }  
+  
+  public setEngineCapacity(engineCapacity: number) {
+    this.engineCapacity = engineCapacity;
+  }
+
+  public getEngineCapacity() {
+    return this.engineCapacity;
+  }
+}
+
+export default Motocycle;
