1905e4b1a987dfd3d84a32fc39be6daef5daafa0
mirelladourado <mirellaalvesdourado@gmail.com>
2023-04-29 16:27:51 -0300

    feat: (Requisito 2) CarRouter - rota post getAll

--- src/Routers/CarRoute.ts
+++ src/Routers/CarRoute.ts
@@ -5,4 +5,6 @@ const carRouter = Router();
 
 carRouter.post('/', (req: Request, res: Response) => new CarController(req, res).create());
 
+carRouter.get('/', (req: Request, res: Response) => new CarController(req, res).getAll());
+
 export default carRouter;
