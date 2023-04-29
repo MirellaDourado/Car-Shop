94ea243004c98e02e9fec9257c8683d971a43dfa
mirelladourado <mirellaalvesdourado@gmail.com>
2023-04-29 15:24:27 -0300

    feat: (Requisito 1) CarODM e Interface

---
+++ src/Interfaces/ICar.ts
@@ -0,0 +1,10 @@
+export default interface ICar {
+  id: number | undefined
+  model: string
+  year: number
+  color: string
+  status: boolean
+  buyValue: number
+  doorsQty: number
+  seatsQty: number
+}
---
+++ src/Models/CarODM.ts
@@ -0,0 +1,48 @@
+import {
+  Model,
+  Schema,
+  model,
+  models,
+} from 'mongoose';
+import ICar from '../Interfaces/ICar';
+  
+export default class CarODM {
+  private schema: Schema;
+  private model: Model<ICar>;
+
+  constructor() {
+    this.schema = new Schema<ICar>(
+      {
+        model: { type: String, required: true },
+        year: { type: Number, required: true },
+        color: { type: String, required: true },
+        status: { type: Boolean },
+        buyValue: { type: Number, required: true },
+        doorsQty: { type: Number, required: true },
+        seatsQty: { type: Number, required: true },
+      },
+      {
+        toJSON: {
+          transform(doc, ret) {
+            const transformed = {
+              id: ret._id,
+              buyValue: ret.buyValue,
+              seatsQty: ret.seatsQty,
+              doorsQty: ret.doorsQty,
+              color: ret.color,
+              model: ret.model,
+              year: ret.year,
+              status: ret.status,
+            };
+            return transformed;
+          },
+        },
+      },
+    );
+    this.model = models.Car || model('Car', this.schema);
+  }
+
+  public async create(car: ICar): Promise<ICar> {
+    return this.model.create({ ...car });
+  }
+}
